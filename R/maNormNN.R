a469bc8ba1660bb1fade73bb187d73c4c8a89f93
Adi Tarca <atarca@med.wayne.edu>
2007-03-31 21:20:23 +0000

    new version
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/nnNorm@23566 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/maNormNN.R
+++ R/maNormNN.R
@@ -5,7 +5,7 @@ require(nnet)
 require(marray)
 
 
-maNormNN<-function(mbatch,binWidth=3,binHeight=3,model.nonlins=3,iterations=200,robust=TRUE,maplots=FALSE, verbose=FALSE) 
+maNormNN<-function(mbatch,w=NULL,binWidth=3,binHeight=3,model.nonlins=3,iterations=100,nFolds=10,maplots=FALSE,verbose=FALSE) 
 {
  #see what kind of object is mbatch and initialize a new normalized one "mbatchn"
  if (class(mbatch)=="marrayRaw") {
@@ -15,22 +15,22 @@ maNormNN<-function(mbatch,binWidth=3,binHeight=3,model.nonlins=3,iterations=200,
  }else stop("Mbatch must be an object of type marrayRaw or marrayNorm")
 
  # check validity of binWidth
- if ((binWidth<=maNsc(mbatch))&&(binWidth>=1))
+ if(is.null(binWidth)){nbx=1} else if ((binWidth<=maNsc(mbatch))&&(binWidth>=1))
     {nbx<-floor(maNsc(mbatch)/binWidth)
- }else stop(paste("binWidth must be an integer superior to 1 and inferior to ",maNsc(mbatch))) 
+ }else stop(paste("binWidth must be either NULL or an integer superior to 1 and inferior to ",maNsc(mbatch),"\n")) 
 
  # check validity of binHeight
- if ((binHeight<=maNsr(mbatch))&&(binHeight>=1))
+ if(is.null(binHeight)){nby=1}else if ((binHeight<=maNsr(mbatch))&&(binHeight>=1))
     {nby<-floor(maNsr(mbatch)/binHeight)
- }else stop(paste("binHeight must be an integer superior to 1 and inferior to ",maNsr(mbatch))) 
+ }else stop(paste("binHeight must be either NULL or an integer superior to 1 and inferior to ",maNsr(mbatch),"\n")) 
 
  #decide whether or not to use the spatial coordinates, function of nbx and nby vals.
  vectd<-NULL
  if (nbx>=2) {vectd<-c(vectd,1)
- }else paste("binWidth being too large, the space coordinate X was not used in normalization")
+ }else cat(paste("binWidth being too large or NULL, the space coordinate X was not used in normalization","\n"))
 
- if (nby>=2) {vectd<-c(vectd,2)
- }else paste("binHeight being too large, the space coordinate Y was not used in normalization")
+ if (nby>=2){vectd<-c(vectd,2)
+ }else cat(paste("binHeight being too large or NULL, the space coordinate Y was not used in normalization","\n"))
  vectd<-c(vectd,3)
  
  #check validity of model.nonlins
@@ -38,13 +38,38 @@ maNormNN<-function(mbatch,binWidth=3,binHeight=3,model.nonlins=3,iterations=200,
     {nodes<-model.nonlins
  }else stop(paste("model.nonlins must be an integer superior to 0 and inferior to ",20)) 
 
+ #check validity of ncv
+ if ((nFolds>=3)&&(nFolds<=50))
+    {ncv=nFolds
+ }else stop(paste("model.nonlins must be an integer superior to 0 and inferior to ",50)) 
+
+
  #check validity of iterations
  if ((iterations>=50)&&(iterations<=500))
     {ite<-iterations
  }else stop(paste("iterations must be an integer superior to 50 and inferior to ",500)) 
+ 
+#check validity of w
+ if(is.null(w)){
+ w=rep(1,dim(maM(mbatch))[1])
+ } else if(length(w)!=maNspots(mbatch)){stop("the weights vector w should have the length maNspots(mbatch)!!!")}
 
 
- #define bins limits on X and Y direction on the print-tip group
+ nv<-length(vectd)
+ nreps=5
+
+ set.seed(1234) #require a fixed seed to obtain always the same normalized results 
+ 
+#define some functions
+ norm01<-function(x){(x-min(x,na.rm=TRUE))/(max(x,na.rm=TRUE)-min(x,na.rm=TRUE))} 
+ norm28<-function(x){((x-min(x,na.rm=TRUE))/(max(x,na.rm=TRUE)-min(x,na.rm=TRUE)))*0.6+0.2} 
+ denorm28<-function(x,mn,mx){
+   a<-x*1.0/0.6-1.0/3.0;
+   (a*(mx-mn)+mn); 
+ }
+
+ 
+ #define bin limits on X and Y direction on the print-tip group
  bXc<-seq(0,maNsc(mbatch),length=(nbx+1))
  bYr<-seq(0,maNsr(mbatch),length=(nby+1))
  breaksXc<-as.integer(bXc); XcLev<-seq(0,1,length=nbx);
@@ -52,154 +77,54 @@ maNormNN<-function(mbatch,binWidth=3,binHeight=3,model.nonlins=3,iterations=200,
 
  NSlides<-maNsamples(mbatch)           #get the number of slides in batch
  Npt<-maNgr(mbatch)*maNgc(mbatch)      #get the number of print tips
- Nspots<-maNspots(mbatch)          #get the number of spots per slide
- set.seed(200) #require a fixed seed to obtain always the same normalized results 
 
  for (s in 1:NSlides){                                        #for each slide
    cat(paste("\n","Processing array ",s," of ",NSlides,"\n",sep=""));
    for (pt in 1:Npt){                                         #for each print Tip
         cat("*");
-        #get positions in each sector/tip group
+
+        #get positions in each print tip
         ind<-maPrintTip(mbatch[,s])==pt 
-        A<-maA(mbatch[ind,s])                                #get M (log ratios)
+        A<-maA(mbatch[ind,s])	                             #get M (log ratios)
         M<-maM(mbatch[ind,s])                              #get A (log intensity)
         xc<-factor(cut(maSpotCol(mbatch[ind,s]), breaks = breaksXc))
         yr<-factor(cut(maSpotRow(mbatch[ind,s]), breaks = breaksYr))
         levels(xc)<-XcLev;
         levels(yr)<-YrLev;
         #buid the predictors-target matrix
-        Xy<-cbind(xc,yr,A,M);
-        Xyclean<-na.omit(Xy);   #remove NaN
-        XLst<-norm01(Xyclean[,1:3]); #normalize predictors to be in interval 0_1 
-      yLst<-norm28(cbind(Xyclean[,4])); #normalize target to be in interval 0.2_0.8
-        yn<-yLst$Xn;
-      Xn<-XLst$Xn;
-        #compute weigths for samples
-        bins<-20;  nv<-length(vectd)
-        breaksA <-seq(0, dim(Xn)[1],length=(bins+1))
-        ax<-factor(cut(1:dim(Xn)[1],breaks=breaksA))
-        levels(ax)<-1:bins
-        ord<-order(Xn[,3])
-      Mat<-cbind(Xn[ord,],yn[ord],ax)
-        weigthsamp<-NULL;
-      for (i in 1:bins)
-        { Minbini<-Mat[Mat[,5]==i,4]
-            weigthi<-tricube(Minbini)
-          weigthsamp<-c(weigthsamp, weigthi)
+        Xy<-cbind(xc,yr,A,M);colnames(Xy)<-c("xc","yr","A","M")
+        Xyn<-cbind(apply(Xy[,c("xc","yr","A")],2,norm01),norm28(Xy[,"M"]))
+        colnames(Xyn)<-c("rxc","ryr","rA","rM")
+
+        sbs<-sample(rep(1:ncv,dim(Xyn)[1]/(ncv-1))[1:(dim(Xyn)[1])])  
+        prd<-matrix(NA,dim(Xyn)[1],nreps);
+
+        for(rps in 1:nreps){
+        for(cv in 1:ncv){        
+         ssetT<-(1:(dim(Xyn)[1]))[sbs!=cv]
+         ssetG<-(1:(dim(Xyn)[1]))[sbs==cv]
+         if(nv==3){        
+         nety1 <- nnet(rM~rxc+ryr+rA,data=Xyn, subset=ssetT,weights=w[ind], size = nodes, rang = 0.5,
+               maxit = ite,reltol=0.75e-7,trace=verbose)
+         }else if(nv==1){
+          nety1 <- nnet(rM~rA,data=Xyn, subset=ssetT,weights=w[ind], size = nodes, rang = 0.5,
+               maxit = ite,reltol=0.75e-7,trace=verbose)
+         }else if(sum(vectd==c(1,3))==2){
+          nety1 <- nnet(rM~rxc+rA,data=Xyn, subset=ssetT,weights=w[ind], size = nodes, rang = 0.5,
+               maxit = ite,reltol=0.75e-7,trace=verbose)       
+         }else {
+          nety1 <- nnet(rM~ryr+rA,data=Xyn, subset=ssetT,weights=w[ind], size = nodes, rang = 0.5,
+               decay, maxit = ite,reltol=0.75e-7,trace=verbose)       
          }
-      #end compute weights
-        len<-dim(Mat)[1]
-        aS<-1:len; 
-        smp1<-sample(aS,round(len/5)) 
-        smp2<-sample(aS[-smp1],round(len/5)) 
-        smp3<-sample(aS[-c(smp1,smp2)],round(len/5)) 
-        smp4<-sample(aS[-c(smp1,smp2,smp3)],round(len/5))
-        smp5<-aS[-c(smp1,smp2,smp3,smp4)] 
+         yen<-predict(nety1,Xyn[ssetG,])
+         prd[sbs==cv,rps]<-denorm28(yen,mx=max(M,na.rm=TRUE),mn=min(M,na.rm=TRUE))
 
-        s2345<-c(smp2,smp3,smp4,smp5);
-        s1345<-c(smp1,smp3,smp4,smp5);
-        s1245<-c(smp1,smp2,smp4,smp5); 
-        s1235<-c(smp1,smp2,smp3,smp5); 
-        s1234<-c(smp1,smp2,smp3,smp4);
-         
-      if(!robust){weigthsamp<-rep(1,length(weigthsamp))}
-      estm<-NULL;
-     for (rr in 1:4){ #get 4 estimates
-        trials<-1; mm<-1;
-        while((trials<=20)&(mm>0.5)){
-          nety1 <- nnet(cbind(Mat[s2345,vectd]), Mat[s2345,4], weights=weigthsamp[s2345],size = nodes, rang = 0.5,
-                decay=1e-4, maxit = ite,reltol=0.75e-7,trace=verbose)
-          yen<-NULL; 
-          yen<-predict(nety1,cbind(Xn[-s2345,vectd]))
-          Mfit<-denorm28(yen,yLst$mins,yLst$maxs)
-         Mfitted<-cbind(Mfit[[1]])
-          mm<-abs(median(Xyclean[-s2345,4]-Mfitted)) #compute the mean of normalized M values, to know if all is ok
-          trials<-trials+1;
-         } 
-        if(trials>=20){warning(paste("At slide #",s," and subarray #",pt," there was a convergence problem.",sep=""))}   
-
-        trials<-1; mm<-1;
-        while((trials<=20)&(mm>0.5)){
-          nety2 <- nnet(cbind(Mat[s1345,vectd]), Mat[s1345,4], weights=weigthsamp[s1345],size = nodes, rang = 0.5,
-                decay=1e-4, maxit = ite,reltol=0.75e-7,trace=verbose)
-          yen<-NULL; 
-          yen<-predict(nety2,cbind(Xn[-s1345,vectd]))
-          #and denormalize them back
-          Mfit<-denorm28(yen,yLst$mins,yLst$maxs)
-         Mfitted<-cbind(Mfit[[1]])
-          mm<-abs(median(Xyclean[-s1345,4]-Mfitted)) #compute the mean of normalized M values, to know if all is ok
-          trials<-trials+1;
-         } 
-         if(trials>=20){warning(paste("At slide #",s," and subarray #",pt," there was a convergence problem.",sep=""))}   
-
-        trials<-1; mm<-1;
-        while((trials<=20)&(mm>0.5)){
-          nety3 <- nnet(cbind(Mat[s1245,vectd]), Mat[s1245,4], weights=weigthsamp[s1245],size = nodes, rang = 0.5,
-                decay=1e-4, maxit = ite,reltol=0.75e-7,trace=verbose)
-          yen<-NULL; 
-          yen<-predict(nety3,cbind(Xn[-s1245,vectd]))
-          #and denormalize them back
-          Mfit<-denorm28(yen,yLst$mins,yLst$maxs)
-         Mfitted<-cbind(Mfit[[1]])
-          mm<-abs(median(Xyclean[-s1245,4]-Mfitted)) #compute the mean of normalized M values, to know if all is ok
-          trials<-trials+1;
         }
-        if(trials>=20){warning(paste("At slide #",s," and subarray #",pt," there was a convergence problem.",sep=""))}   
-
-        trials<-1; mm<-1;
-        while((trials<=20)&(mm>0.5)){
-          nety4 <- nnet(cbind(Mat[s1235,vectd]), Mat[s1235,4], weights=weigthsamp[s1235],size = nodes, rang = 0.5,
-                decay=1e-4, maxit = ite,reltol=0.75e-7,trace=verbose)
-          yen<-NULL;
-          yen<-predict(nety4,cbind(Xn[-s1235,vectd]))
-          #and denormalize them back
-          Mfit<-denorm28(yen,yLst$mins,yLst$maxs)
-         Mfitted<-cbind(Mfit[[1]])
-          mm<-abs(median(Xyclean[-s1235,4]-Mfitted)) #compute the mean of normalized M values, to know if all is ok
-          trials<-trials+1;
-        }
-        if(trials>=20){warning(paste("At slide #",s," and subarray #",pt," there was a convergence problem.",sep=""))}   
-       
-         trials<-1; mm<-1;
-         while((trials<=20)&(mm>0.5)){
-          nety5 <- nnet(cbind(Mat[s1234,vectd]), Mat[s1234,4], weights=weigthsamp[s1234],size = nodes, rang = 0.5,
-                decay=1e-4, maxit = ite,reltol=0.75e-7,trace=verbose)
-          yen<-NULL;
-          yen<-predict(nety5,cbind(Xn[-s1234,vectd]))
-          #and denormalize them back
-          Mfit<-denorm28(yen,yLst$mins,yLst$maxs)
-         Mfitted<-cbind(Mfit[[1]])
-          mm<-abs(median(Xyclean[-s1234,4]-Mfitted)) #compute the mean of normalized M values, to know if all is ok
-          trials<-trials+1;
        }
-        if(trials>=20){warning(paste("At slide #",s," and subarray #",pt," there was a convergence problem.",sep=""))}   
+       
+       
+       maM(mbatchn)[ind,s]<- M-apply(prd,1,median,na.rm=TRUE)
 
-       
-       
-        # get the fitted values 
-        yen<-array(dim=len) 
-        yen[-s2345]<-predict(nety1,cbind(Xn[-s2345,vectd]))
-        yen[-s1345]<-predict(nety2,cbind(Xn[-s1345,vectd]))
-        yen[-s1245]<-predict(nety3,cbind(Xn[-s1245,vectd]))
-        yen[-s1235]<-predict(nety4,cbind(Xn[-s1235,vectd]))
-        yen[-s1234]<-predict(nety5,cbind(Xn[-s1234,vectd]))
-        #and denormalize them back
-        Mfit<-denorm28(yen,yLst$mins,yLst$maxs)
-        Mfitted<-cbind(Mfit[[1]])
-        Mfit<-Mfitted;
-        
-        # insert NaNs where they where in the original M vector if is the case
-         NAloc<-attr(Xyclean,"na.action")
-        if (length(NAloc)>0){
-          yelong<-matrix(NA,dim(M)[1],1)
-          yelong[-NAloc,1]<-Mfit
-        }else{yelong<-Mfit }
-        estm<-cbind(estm,yelong);
-      } #end get 3 estimates
-      meann<-function(x){if(sum(is.na(x))>0){NA}else{mean(x,trim=0.25)}}
-      yelong<-apply(estm,1,mean);
-      #normalizing by substracting the predicted values
-       maM(mbatchn)[ind,s]<-cbind(M)-yelong
 
    } #end with print tip 
    if(maplots) {
